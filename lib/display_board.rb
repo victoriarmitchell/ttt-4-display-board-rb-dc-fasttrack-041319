5d578127073eaf11da0a7d5bb6715040dbd5107c
Victoria M <victoria.raq.mitchell@gmail.com>
2019-04-13 18:00:20 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -7,7 +7,7 @@ def display_board(board = [" "," "," "," "," "," "," "," "," "])
   puts "-----------"
   puts " #{board[3]} | #{board[4]} | #{board[5]} "
   puts "-----------"
-  puts " #{board[6]} | #{board[7]} | #{board[8]}  "
+  puts " #{board[6]} | #{board[7]} | #{board[8]} "
 end
 
 display_board(["X","X","X","X","X","X","X","X","X"])
