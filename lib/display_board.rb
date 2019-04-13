4159db18fd38a2b503701d0733e601bba8a38647
Victoria M <victoria.raq.mitchell@gmail.com>
2019-04-13 17:54:32 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,2 +1,14 @@
 # Define display_board that accepts a board and prints
 # out the current state.
+
+# Define a method display_board that prints a 3x3 Tic Tac Toe Board
+def display_board(board)
+  puts "   |   |   "
+  puts "-----------"
+  puts "   |   |   "
+  puts "-----------"
+  puts "   |   |   "
+end
+
+display_board
+
