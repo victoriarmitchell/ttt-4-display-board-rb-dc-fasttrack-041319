741890d55fd78b0dcdd756e11b83fc0458297b7a
Victoria M <victoria.raq.mitchell@gmail.com>
2019-04-13 17:58:46 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -2,7 +2,7 @@
 # out the current state.
 
 # Define a method display_board that prints a 3x3 Tic Tac Toe Board
-def display_board(board = ["X"," "," "," "," "," "," "," "," "])
+def display_board(board = [" "," "," "," "," "," "," "," "," "])
   puts " #{board[0]}  | #{board[1]}  | #{board[2]}  "
   puts "-----------"
   puts " #{board[3]}  | #{board[4]}  | #{board[5]}  "
